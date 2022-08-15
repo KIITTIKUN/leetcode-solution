b11636eca84927234aec271623cefd169f45bf27
KIITTIKUN <kittikun.real@gmail.com>
2022-08-15 10:29:38 +0700

    feat(moveZeroes): solve

--- check-if-number-has-equal-digit-count-and-digit-value.js
+++ check-if-number-has-equal-digit-count-and-digit-value.js
@@ -6,8 +6,6 @@ const digitCount = (num) => {
   }
   for (let i = 0; i < digits.length; i++) {
     if (dataCount[i] !== parseInt(digits[i])) {
-      console.log(dataCount);
-      console.log(digits);
       return false;
     }
   }
---
+++ moveZeroes.js
@@ -0,0 +1,26 @@
+const moveZeroes = (integerNumbers) => {
+  let addZeroes = integerNumbers.length;
+  const indexOfZeroes = [];
+  if (integerNumbers.indexOf(0) === -1) {
+    return integerNumbers;
+  }
+  for (let i = 0; i < integerNumbers.length; i++) {
+    if (integerNumbers[i] === 0) {
+      indexOfZeroes.push(i);
+    }
+  }
+  indexOfZeroes.sort((a, b) => {
+    return b - a;
+  });
+  for (let i = 0; i < indexOfZeroes.length; i++) {
+    integerNumbers.splice(indexOfZeroes[i], 1);
+  }
+
+  addZeroes = addZeroes - integerNumbers.length;
+  for (let i = 0; i < addZeroes; i++) {
+    integerNumbers.push(0);
+  }
+  return integerNumbers;
+};
+
+console.log(moveZeroes([0, 0, 1]));
