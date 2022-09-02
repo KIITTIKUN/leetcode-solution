101d7a42208b31a468b6bf68ea43d5444220eb05
KIITTIKUN <kittikun.real@gmail.com>
2022-09-02 11:38:46 +0700

    feat(happy number): solve

---
+++ happy-number.js
@@ -0,0 +1,16 @@
+const isHappyNumber = (number) => {
+  while (true) {
+    if (number == 1) {
+      return true;
+    } else if (number > 1 && number < 7) {
+      return false;
+    }
+    let newNumber = 0;
+    while (number > 0) {
+      newNumber = newNumber + (number % 10) ** 2;
+      number = Math.floor(number / 10);
+    }
+    number = newNumber;
+  }
+};
+console.log(isHappyNumber(7));
