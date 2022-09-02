4a1cfd8c3bbe35fb48aa155e00c5b1d2ded34547
KIITTIKUN <kittikun.real@gmail.com>
2022-09-02 11:18:52 +0700

    feat(palindrome): solve

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+const isPalindromeNumber = (number) => {
+  let copyNumber = number;
+  let reverseNumber = 0;
+  while (copyNumber > 0) {
+    reverseNumber = reverseNumber * 10 + (copyNumber % 10);
+    copyNumber = Math.floor(copyNumber / 10);
+  }
+  return (number >= 0 && number < 10) || reverseNumber == number;
+};
+
+console.log(isPalindromeNumber(5));
